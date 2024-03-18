16762627d3812447c632468977f455f95c0e511a
Scott Schulte <157662548+sschulte24110@users.noreply.github.com>
2024-03-18 10:39:55 -0500

    Addition of basic Math operators to script.js.
    Minor changes to css and html.

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// const button = document.querySelectorAll('button');
+//     button.addEventListener('click', )
+
+
+// Operation functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+console.log(subtract(9, 3));
+
+// const display = document.querySelector('.display');
+
+// keys.addEventListener('click', e => {
+//     if (e.target.matches('button')) {
+//         const key = e.target
+//         const action = key.dataset.action
+//         const key
+//     }
+// })
